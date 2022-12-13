7ed4e9a6a6f6d34b8003af9d697090012b1c76c6
Miro Rihu <100125793+Kiikuri@users.noreply.github.com>
2022-12-13 10:05:13 +0200

    Update NETGEAR_A6100_driver_installer.sh

--- NETGEAR_A6100_driver_installer.sh
+++ NETGEAR_A6100_driver_installer.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#check for root
 UID=$(id -u)
 if [ x$UID != x0 ] 
 then
@@ -7,38 +8,36 @@ then
     exec sudo su -c "bash $cmd_str"
 fi
 
-set -e
-slp=2
+export DEBIAN_FRONTEND=noninteractive
+slp=0 
 
+# exit when any command fails
+set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo -e "\"${last_command}\" command filed with exit code $?."; echo -e "Installer: Installation Failed"; echo -e "Try rebooting if the RTL8812AU driver was not installed; dkms status"' ERR
 
-echo "Installer: Installing RTL8812AU drivers for NETGEAR A6100/AC600"
+echo 'Installer: Installing RTL8812AU drivers for NETGEAR A6100/AC600'
+echo 'Installer: Running apt-get update & upgrade & dist-upgrade'
 sleep $slp
-echo "Installer: Running apt-get update & upgrade and dist-upgrade"
+apt-get update -y && apt-get upgrade -y && apt dist-upgrade -y
+echo 'Installer: Installing headers'
 sleep $slp
-DEBIAN_FRONTEND=noninteractive apt-get update -y && DEBIAN_FRONTEND=noninteractive apt-get upgrade -y && DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
-echo "Installer: Installing headers"
-sleep $slp
-DEBIAN_FRONTEND=noninteractive apt-get install linux-headers-$(uname -r) -y
-echo "Installer: Installing Git"
+echo apt-get install linux-headers-$(uname -r) -y
+echo 'Installer: Installing Git'
 sleep $slp
 apt-get install git -y
-echo "Installer: Getting the installation files"
+echo 'Installer: Getting the installation files'
 sleep $slp
 rm -rf rtl8812au
 git clone https://github.com/aircrack-ng/rtl8812au
-echo "Installer: Installing dkms"
+echo 'Installer: Installing dkms'
 sleep $slp
-DEBIAN_FRONTEND=noninteractive apt-get install dkms -y
+apt-get install dkms -y
+echo 'Installer: Installing drivers'
+sleep $slp
+rm -r /var/lib/dkms/8812au ||:
 cd rtl8812au
-echo "Installer: Installing drivers"
-sleep $slp
-rm -r /var/lib/dkms/8812au
 make dkms_install
-dkms status
-echo "Installer: Installation complete!"
-
-
+echo 'Installer: Installation complete!'
